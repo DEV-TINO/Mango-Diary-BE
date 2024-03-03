00b59b78cf2320436df8b516e3121564de9bb0cd
kim-song-jun <>
2024-03-03 15:41:14 +0900

    fix: fix path

--- routers/post.js
+++ routers/post.js
@@ -7,7 +7,7 @@ const path = require('path')
 
 
 const createDirectory = ({post_type: post_type}) => {
-  const staticPath = path.join('/data', 'uploadImages')
+  const staticPath = '/code/public/images/uploads'
   const postPath = path.join(staticPath, post_type)
   try {
     if (!fs.existsSync(postPath)) {
